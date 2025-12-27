848f9d5ef29f9364b3cf170eda50f54e023b9d1a
zapsys <zapsyssolutions@gmail.com>
2025-12-27 17:10:27 -0300

    Import router file config

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import AOS from 'aos'
+import router from './router'
 
 AOS.init({
     easing: 'ease-in-out'
 })
 
 createApp(App)
+    .use(router)
     .mount('#app')
