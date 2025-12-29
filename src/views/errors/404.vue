93c505dae03631454dffef482b774b7808d49827
zapsys <zapsyssolutions@gmail.com>
2025-12-28 22:46:23 -0300

    Added class `rounded-full`

--- src/views/errors/404.vue
+++ src/views/errors/404.vue
@@ -2,6 +2,6 @@
     <div class="flex flex-col items-center justify-center min-h-screen bg-base-200">
         <h1 class="text-5xl font-bold">404</h1>
         <p class="mt-4 text-xl">Página não encontrada</p>
-        <a href="/" class="mt-6 btn btn-primary">Voltar para a página inicial</a>
+        <a href="/" class="mt-6 btn btn-primary rounded-full">Voltar para a página inicial</a>
     </div>
 </template>
